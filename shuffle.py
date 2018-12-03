18c5d5cb13803b1acb11825aaf749e62268909f0
Samuel Wolf <samuelx5n@yahoo.de>
2018-12-03 19:27:50 +0100

    ADS 6/3

---
+++ shuffle.py
@@ -0,0 +1,27 @@
+from random import randint
+
+
+def shuffle(lst):
+    n = (len(lst)+1) // 2
+    l = [i for i in lst[:n]]
+    r = [i for i in lst[n:]]
+
+    i, j, m = 0, 0, 0
+    for k in range(len(lst)):
+        if (randint(0, 1) and i < n) or j >= n-1:   # [0, 1, 1, 1, 0, 0, 1, 0, 1][m] == 0
+            lst[k] = l[i]
+            i += 1
+        else:
+            lst[k] = r[j]
+            j += 1
+        # m += 1
+
+
+def main():
+    lst = [4, 3, 2, 7, 9, 1, 8, 5, 6]
+    shuffle(lst)
+    print(lst)
+
+
+if __name__ == '__main__':
+    main()
