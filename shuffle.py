aba4b793e6f2e593a64da775e318bfbec8e3c3ee
Simon Sacher <simonsacher@gmail.com>
2018-12-04 15:37:16 +0100

    changed shuffle.py to feature len(r)

--- shuffle.py
+++ shuffle.py
@@ -8,7 +8,7 @@ def shuffle(lst):
 
     i, j, m = 0, 0, 0
     for k in range(len(lst)):
-        if (randint(0, 1) and i < n) or j >= n-1:   # [0, 1, 1, 1, 0, 0, 1, 0, 1][m] == 0
+        if (randint(0, 1) and i < n) or j >= len(r):  # if [0, 1, 1, 1, 0, 0, 1, 0, 1][m] == 0 or j >= n-1:
             lst[k] = l[i]
             i += 1
         else:
@@ -19,6 +19,7 @@ def shuffle(lst):
 
 def main():
     lst = [4, 3, 2, 7, 9, 1, 8, 5, 6]
+    lst = [1, 2, 3, 4, 6, 7]
     shuffle(lst)
     print(lst)
 
