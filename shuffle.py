7f460cacaedc5c89e4eb9ca4a9868791f2cc8e0e
Korogi <samuelx5n@yahoo.de>
2018-12-04 15:59:40 +0100

    unshuffle

--- shuffle.py
+++ shuffle.py
@@ -17,11 +17,34 @@ def shuffle(lst):
         # m += 1
 
 
+def unshuffle(lst):
+    n = (len(lst)+1) // 2
+    l, r = [], []
+    m = 3
+
+    for element in lst:
+        if element <= m:
+            l.append(element)
+        else:
+            r.append(element)
+
+    return l + r
+
+
 def main():
-    lst = [4, 3, 2, 7, 9, 1, 8, 5, 6]
+    # lst = [4, 3, 2, 7, 9, 1, 8, 5, 6]
+
     lst = [1, 2, 3, 4, 6, 7]
     shuffle(lst)
-    print(lst)
+    print(unshuffle(lst))
+
+    for _ in range(10000):
+        lst = [1, 2, 3, 4, 6, 7]
+        shuffle(lst)
+        if unshuffle(lst) == [1, 2, 3, 4, 6, 7]:
+            continue
+        else:
+            print(False)
 
 
 if __name__ == '__main__':
