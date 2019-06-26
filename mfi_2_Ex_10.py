536d86bb75db21235d22f1858ce07fe8091e077a
Korogi <samuelx5n@yahoo.de>
2019-06-26 19:38:36 +0200

    renamed func ex_one_a -> ex_one_b, ex_two added

--- mfi_2_Ex_10.py
+++ mfi_2_Ex_10.py
@@ -1,4 +1,5 @@
-from random import randint
+from random import randint, shuffle
+from math import factorial
 
 
 def omega():
@@ -11,7 +12,7 @@ def omega():
     return omega
 
 
-def ex_one_a(bool_test, calc=None, n=1000000):
+def ex_one_b(bool_test, calc=None, n=1000000):
     hit = 0
 
     for _ in range(n):
@@ -32,15 +33,55 @@ def ex_one_c(bool_test_1, bool_test_2, calc=None, n=1000000):
     print(f"simulation: {hit/n}, calc: {calc}")
 
 
-print(f"omega: {len(omega())}")
+def ex_two(n=1000000):
+    def choose():
+        markets = [0] * 20 + [1] * 10
+        stores = []
+
+        for i in range(6):
+            rand = randint(0, 29-i)
+            stores.append(markets[rand])
+            del markets[rand]
+
+        return stores
+
+    def calc():
+        comb = lambda n, k: factorial(n)/(factorial(k)*factorial(n-k))
+        return 1 - sum([comb(6, k)*(factorial(24)/factorial(30))*(factorial(20)/factorial(20-6+k))*
+                        (factorial(10)/factorial(10-k)) for k in range(3)])
+
+    success = 0
+
+    for _ in range(n):
+        sample = choose()
+        success += sum(sample) >= 3
+
+    print(f"simulation: {success / n} calc: {calc()}")
+
+
+def calc_2():
+    a = [30-i for i in range(6)]
+    b = [20-i for i in range(6)]
+    mult = 1
+
+    for x, y in zip(b, a):
+        mult *= x/y
+
+    print(mult)
+
+
+ex_two()
+
+"""print(f"omega: {len(omega())}")
 print("E1: ", end="")
-ex_one_a(lambda sample: len(list(filter(lambda x: x <= 3, sample))) == 3, (3/10)**3)
+ex_one_b(lambda sample: len(list(filter(lambda x: x <= 3, sample))) == 3, (3 / 10) ** 3)
 print("E2: ", end="")
-ex_one_a(lambda sample: sample[0] <= 3 and sample[1] >= 4 and sample[2] >= 4, (3/10)*(7/10)*(7/10))
+ex_one_b(lambda sample: sample[0] <= 3 and sample[1] >= 4 and sample[2] >= 4, (3 / 10) * (7 / 10) * (7 / 10))
 print("E3: ", end="")
-ex_one_a(lambda sample: sample[0] <= 3, 0.3)
+ex_one_b(lambda sample: sample[0] <= 3, 0.3)
 print("E1 or E2: ", end="")
 ex_one_c(lambda sample: len(list(filter(lambda x: x <= 3, sample))) == 3,
          lambda sample: sample[0] <= 3 and sample[1] >= 4 and sample[2] >= 4, (3/10)**3 + (3/10)*(7/10)*(7/10))
 print("E2 or E3: ", end="")
-ex_one_a(lambda sample: sample[0] <= 3 or sample[1] <= 3 or sample[2] <= 3, 1 - (7/10) ** 3)
+ex_one_b(lambda sample: sample[0] <= 3 or sample[1] <= 3 or sample[2] <= 3, 1 - (7 / 10) ** 3)
+"""
